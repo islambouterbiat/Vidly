ed7d0d532e79cab408038fae0c153e342549d4dc
unknown <islambouter2000@gmail.com>
2020-04-25 03:50:24 +0200

    user registeration

--- src/components/common/registerform.jsx
+++ src/components/common/registerform.jsx
@@ -1,31 +1,39 @@
-import React, { Component } from "react";
+import React from "react";
+import * as userService from "../../services/userService";
 import Joi from "joi-browser";
 import Form from "./form";
 
 class RegisterForm extends Form {
   state = {
-    data: { username: "", fullname: "", password: "" },
+    data: { username: "", password: "", name: "" },
     errors: {},
   };
 
   schema = {
     username: Joi.string().email().required().label("Username"),
-    fullname: Joi.string().required().label("Fullname"),
     password: Joi.string().required().min(6).label("Password"),
+    name: Joi.string().required().label("Fullname"),
   };
 
-  doSubmit = () => {
-    //backend
-    console.log("submitted");
+  doSubmit = async () => {
+    try {
+      await userService.register(this.state.data);
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = ex.response.data;
+        this.setState({ errors });
+      }
+    }
   };
   render() {
     return (
       <div>
         <h1>Register</h1>
-        <form onSubmit={this.handleLoginSubmit}>
+        <form onSubmit={this.handleSubmit}>
           {this.renderInput("username", "Username")}
-          {this.renderInput("fullname", "Fullname")}
           {this.renderInput("password", "Password", "password")}
+          {this.renderInput("name", "Fullname")}
         </form>
         {this.renderButton("Register")}
       </div>
