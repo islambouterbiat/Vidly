68fa21979eca50f131fd70934e321b72086571ac
unknown <islambouter2000@gmail.com>
2020-04-25 23:26:11 +0200

    fixing Bi directional dependencies

--- src/services/authService.js
+++ src/services/authService.js
@@ -5,6 +5,8 @@ import jwtDecode from "jwt-decode";
 const apiEndPoint = apiUrl + "auth";
 const tokenKey = "token";
 
+http.setJwt(getJwt());
+
 export async function login(email, password) {
   const { data: jwt } = await http.post(apiEndPoint, { email, password });
   localStorage.setItem(tokenKey, jwt);
@@ -27,9 +29,14 @@ export function getCurrentUser() {
   }
 }
 
+export function getJwt() {
+  return localStorage.getItem(tokenKey);
+}
+
 export default {
   login,
   logout,
   getCurrentUser,
   loginWithJwt,
+  getJwt,
 };
