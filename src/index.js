891c601548276ef99b30af23c8c6f6507e19d3ee
unknown <islambouter2000@gmail.com>
2020-04-26 04:52:14 +0200

    envirement variables

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 import App from "./App";
 
+console.log("SUPERMAN", process.env.REACT_APP_NAME);
+
 ReactDOM.render(
   <BrowserRouter>
     <React.StrictMode>
